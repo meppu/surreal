1b13f3b0f17db76a034fffccc51d6e8868e05708
meppu <meppu@proton.me>
2023-05-07 22:36:43 +0300

    add select and from for query builder

---
+++ src/surreal_query.erl
@@ -0,0 +1,24 @@
+-module(surreal_query).
+
+-export([make_one/1]).
+
+make_one(Query) ->
+    Results = lists:map(fun make_one_command/1, Query),
+    WithFormatted = lists:join(" ", Results) ++ ";",
+
+    lists:flatten(WithFormatted).
+
+%% @private
+make_one_command({select}) ->
+    "SELECT *";
+make_one_command({select, Field}) when is_atom(Field) ->
+    io_lib:format("SELECT ~s", [Field]);
+make_one_command({select, Fields}) when is_list(Fields) ->
+    ToString = lists:map(fun atom_to_list/1, Fields),
+    WithFormatted = lists:flatten(lists:join(", ", ToString)),
+
+    io_lib:format("SELECT ~s", [WithFormatted]);
+make_one_command({from, Something}) ->
+    io_lib:format("FROM ~s", [Something]);
+make_one_command(_Other) ->
+    "".
