b37823cb92d7c3aea736107f042137a9d31fe989
meppu <meppu@proton.me>
2023-07-28 00:32:30 +0300

    improve configuration uri format

--- src/surreal_config.erl
+++ src/surreal_config.erl
@@ -8,10 +8,12 @@
 %%-------------------------------------------------------------------------
 -type connection_map() :: #{
     host => unicode:chardata(),
-    path => unicode:chardata(),
-    port => non_neg_integer(),
     username => unicode:chardata(),
-    password => unicode:chardata()
+    password => unicode:chardata(),
+    namespace => unicode:chardata(),
+    database => unicode:chardata(),
+    port => non_neg_integer(),
+    tls => boolean()
 }.
 
 %%-------------------------------------------------------------------------
@@ -28,19 +30,27 @@
 -spec parse(uri()) -> ok() | error().
 parse(Uri) ->
     case uri_string:parse(Uri) of
-        #{host := Host, path := Path, port := Port, scheme := _, userinfo := UserInfo} ->
+        #{
+            host := Host,
+            path := Path,
+            port := Port,
+            scheme := Scheme,
+            userinfo := UserInfo
+        } ->
+            {ok, Tls} = parse_scheme(Scheme),
             {ok, [Username, Password]} = parse_userinfo(UserInfo),
-            #{host => Host, path => Path, port => Port, username => Username, password => Password};
-        #{host := Host, path := Path, scheme := Scheme, userinfo := UserInfo} ->
-            Port =
-                case Scheme of
-                    "https" -> 443;
-                    "wss" -> 443;
-                    _ -> 80
-                end,
+            {ok, [Namespace, Database]} = parse_path(Path),
 
-            {ok, [Username, Password]} = parse_userinfo(UserInfo),
-            #{host => Host, path => Path, port => Port, username => Username, password => Password};
+            #{
+                host => Host,
+                path => Path,
+                username => Username,
+                password => Password,
+                namespace => Namespace,
+                database => Database,
+                port => Port,
+                tls => Tls
+            };
         {error, _, _} = Error ->
             Error;
         _ ->
@@ -55,3 +65,16 @@ parse_userinfo(UserInfo) ->
         [_, _] = Data -> {ok, Data};
         _ -> {error, invalid_userinfo}
     end.
+
+parse_path(Path) ->
+    case string:split(Path, "/", all) of
+        [[], Namespace, Database] -> {ok, [Namespace, Database]};
+        _ -> {error, invalid_path}
+    end.
+
+parse_scheme("surrealdb") ->
+    {ok, false};
+parse_scheme("surrealdb+tls") ->
+    {ok, true};
+parse_scheme(_) ->
+    {error, invalid_scheme}.
